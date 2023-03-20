c82867c8aaf3347984803879c894d8f9a0be69ba
MarkBramblett18 <markbramblett18@gmail.com>
2023-03-20 11:43:32 -0400

    Fix login coloring

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
 
